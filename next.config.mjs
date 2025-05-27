d2c653e0a1180bb0670ade167441b2661708d654
Edrine Mutebi <edrinemutebi7@gmail.com>
2025-05-27 10:23:53 +0300

    build error fix, fix config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,29 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["firebasestorage.googleapis.com", 'lh3.googleusercontent.com'],
+//   },
+// };
+
+
+
+
+// export default nextConfig;
+
+
+
+
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,  // Enable app router
+  },
   images: {
-    domains: ["firebasestorage.googleapis.com", 'lh3.googleusercontent.com'],
+    domains: ["firebasestorage.googleapis.com", "lh3.googleusercontent.com"],
   },
 };
 
-
-
-
 export default nextConfig;
-
-
-
-
-
